47ee5b7468f49e1eb56a871a9bb379614aade995
PrantaChakraborty <cbpranta@gmail.com>
2020-09-22 21:56:39 +0600

    added order models into admin and customized it

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path(r'', views.cart_detail, name='cart_detail'),
-    path(r'(?P/add/<product_id>[-\w])/$', views.cart_add, name='cart_add'),
-    path(r'(?P/remove/<product_id>[-\w]+)/$', views.cart_remove, name='cart_remove'),
+    path(r'(/add/<product_id>[-\w])/', views.cart_add, name='cart_add'),
+    path(r'(/remove/<product_id>[-\w]+)/', views.cart_remove, name='cart_remove'),
 ]
--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,6 @@ from .views import product_list, product_detail
 
 urlpatterns = [
     path(r'', product_list, name='product_list'),
-    path(r'(?P<category_slug>[-\w]+)/$', product_list, name='product_list_by_category'),
-    path(r'(?P<id>\d+)/(?P<slug>[-\w]+)/$', product_detail, name='product_detail'),
+    path(r'(<category_slug>[-\w]+)/', product_list, name='product_list_by_category'),
+    path(r'(<id>\d+)/(<slug>[-\w]+)/', product_detail, name='product_detail'),
 ]
