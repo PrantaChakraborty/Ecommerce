de94410eb118b78048af58dcb5bc94f605f22100
PrantaChakraborty <cbpranta@gmail.com>
2020-09-22 21:45:03 +0600

    added Order and OrderItem models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from shop.models import Product
 
 # Create your models here.
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+
+# used to calculate total price of a user order items
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+# oder for every item
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveBigIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+# used to calculate single item price
+    def get_cost(self):
+        return self.price * self.quantity
