d5bc74eb98e7bebfe9af39aef98f5e88e317674b
PrantaChakraborty <cbpranta@gmail.com>
2020-10-08 21:22:55 +0600

    modified settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'myshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
