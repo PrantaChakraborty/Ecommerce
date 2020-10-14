1ceb6a7b0f32383a0abce86ce5cd97ac395d5f96
PrantaChakraborty <cbpranta@gmail.com>
2020-10-14 20:13:43 +0600

    added Coupon model

--- coupons/models.py
+++ coupons/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)  # coupon code
+    valid_from = models.DateTimeField()  # date for validity
+    valid_to = models.DateTimeField()  # date for validity
+
+    discount = models.IntegerField(validators=[MinValueValidator(0),
+                                               MaxValueValidator(0)])
+    active = models.BooleanField()  # to check active or not
+
+    def __str__(self):
+        return self.code
