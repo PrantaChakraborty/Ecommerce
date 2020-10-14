0a43aa88d057ee1d8b5e932e1dba44805436ce0a
PrantaChakraborty <cbpranta@gmail.com>
2020-10-14 20:28:46 +0600

    added Coupo validity check view

--- coupons/views.py
+++ coupons/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
+from .models import Coupon
+from .forms import CouponApplyForm
 
 # Create your views here.
+
+
+# for checking the coupon code
+@require_POST
+def coupon_apply(request):
+    now = timezone.now()
+    form = CouponApplyForm(request.POST)
+    if form.is_valid():
+        code = form.cleaned_data['code']
+        try:
+            coupon = Coupon.objects.get(code__iexact=code,
+                                        valid_from__lte=now,
+                                        valid_to__gte=now,
+                                        active=True)  # matching the coupon code with the model data
+            request.session['coupon_id'] = coupon.id  # storing id into session
+        except Coupon.DoesNotExist:
+            request.session['coupon_id'] = None
+    return redirect('cart:cart_detail')  # returning data in cart
